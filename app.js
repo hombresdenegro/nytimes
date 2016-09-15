c45193c1231f9f621542dd0d3855a1539064f2cf
RichLira <mac@Ricardos-MacBook-Pro.local>
2016-09-14 21:17:58 -0500

    pinche segundo release

--- app.js
+++ app.js
@@ -12,11 +12,19 @@ myApp.controller('GreetingController', ['$scope' , '$http' , function ($scope, $
         url: 'https://api.nytimes.com/svc/mostpopular/v2/mostviewed/technology/30.json?format=json&query=smoking&api-key=cf83fb399d0c4067a1156c52211312d1'
 
     }).then(function successCallback(response){
-        console.log(response.data.results);
-        $scope.posts = response.data.results;
+        _getResults(response.data.results);
         console.log("exito")
         }, function errorCallback(response){
         console.log("fallo")
     });
+
+    function _getResults(result){
+        $scope.posts = result;
+
+        angular.forEach(result, function(elemento){
+            $scope.imagenes = elemento.media[0]['media-metadata'][1];
+            console.log($scope.imagenes);
+        });
+    }
 
 }]);
